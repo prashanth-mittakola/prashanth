8861e5f1e68af9094feaa40382679feac2b3b843
prashanth-mittakola <mittakolaprashanth@gmail.com>
2022-01-28 12:23:17 +0530

    updated

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,12 +10,10 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Prashanth's homepage" />
         <meta name="author" content="Prashanth Mittakola" />
-        <meta name="author" content="craftzdog" />
+        <meta name="author" content="Prashanth" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
         <meta name="twitter:image" content="/card.png" />
         <meta property="og:site_name" content="Prashanth Mittakola's Homepage" />
         <meta property="og:type" content="website" />
--- components/navbar.js
+++ components/navbar.js
@@ -79,7 +79,7 @@ const Navbar = props => {
           </LinkItem>
           <LinkItem
             _target="_blank"
-            href="https://github.com/craftzdog/craftzdog-homepage"
+            href="https://github.com/prashanth-mittakola/v2"
             path={path}
             display="inline-flex"
             alignItems="center"
