4031b13e1dbb4646594ea592581c00cb2bfb799c
prashanth-mittakola <mittakolaprashanth@gmail.com>
2022-12-10 22:00:52 +0530

    projectsData optimized + fileExplorer

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,63 +3,83 @@ import Layout from '../../components/layouts/article'
 import Section from '../../components/section'
 import Link from 'next/link'
 
+const projectsData = [
+  {
+    title: 'file explorer',
+    link: '/projects/file-explorer',
+    description: 'A simple file explorer using react.js (custom hooks).',
+    newTab: false
+  },
+  {
+    title: 'Apple earphones scroll effect',
+    link: 'https://codepen.io/prashant-mittakola/full/XWjBvZq',
+    description: 'Apple earphones scroll effect.',
+    newTab: true
+  },
+  {
+    title: 'Movies landing page',
+    link: 'https://prashanth-mittakola.github.io/movie-website',
+    description: 'Movies landing page.',
+    newTab: true
+  },
+  {
+    title: 'Covid-19',
+    link: 'https://codepen.io/prashant-mittakola/full/LYRzPPa',
+    description: 'Covid-19 tracker.',
+    newTab: true
+  },
+  {
+    title: 'corona tracker + search filter',
+    link: '/projects/covid/search',
+    description: `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus. Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment. However, some will become seriously ill and require medical attention.`,
+    newTab: false
+  }
+]
+
 const Projects = () => (
   <Layout title="Projects">
     <Container marginTop={'50px'}>
-        <Section>
-        <Link href="/projects/search">
-          <div style={{ background: '#ffffff', textAlign:"center", padding:"30px 25px", cursor:"pointer"}}>
-            <h3 style={{ fontSize: '42px', fontWeight: 'bold', color:"red" }}>1</h3>
-            <h3 style={{fontWeight:700, color:"black",fontSize: '28px', marginBottom:"10px" }}>corona tracker + search filter</h3>
-            <p style={{fontSize:"16px", color:"#a7a7a7" }}>
-              Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.
-              Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment. However, some will become seriously ill and require medical attention.
-            </p>
-          </div>
-        </Link>
-        </Section>
-                
-        <Section>
-        <Link href="https://codepen.io/prashant-mittakola/full/XWjBvZq" >
-        <a target="_blank">
-          <div style={{ background: '#ffffff', textAlign:"center", padding:"30px 25px", cursor:"pointer"}}>
-            <h3 style={{ fontSize: '42px', fontWeight: 'bold', color:"red" }}>2</h3>
-            <h3 style={{fontWeight:700, color:"black",fontSize: '28px', marginBottom:"10px" }}>Apple earphones</h3>
-            <p style={{fontSize:"16px", color:"#a7a7a7" }}>
-            Apple earphones scroll effect.
-            </p>
-          </div>
-        </a>
-        </Link>
-        </Section>
-
-        <Section>
-        <Link href="https://codepen.io/prashant-mittakola/full/LYRzPPa">
-        <a target="_blank">
-          <div style={{ background: '#ffffff', textAlign:"center", padding:"30px 25px", cursor:"pointer"}}>
-            <h3 style={{ fontSize: '42px', fontWeight: 'bold', color:"red" }}>3</h3>
-            <h3 style={{fontWeight:700, color:"black",fontSize: '28px', marginBottom:"10px" }}>Covid-19</h3>
-            <p style={{fontSize:"16px", color:"#a7a7a7" }}>
-              Covid-19 tracker
-            </p>
-          </div>
-          </a>
-        </Link>
-        </Section>
-
-        <Section>
-        <Link href="https://prashanth-mittakola.github.io/movie-website">
-        <a target="_blank">
-          <div style={{ background: '#ffffff', textAlign:"center", padding:"30px 25px", cursor:"pointer"}}>
-            <h3 style={{ fontSize: '42px', fontWeight: 'bold', color:"red" }}>4</h3>
-            <h3 style={{fontWeight:700, color:"black",fontSize: '28px', marginBottom:"10px" }}>landing page</h3>
-            <p style={{fontSize:"16px", color:"#a7a7a7" }}>
-              Movies landing page
-            </p>
-          </div>
-          </a>
-        </Link>
-        </Section>
+      {projectsData.map((item, index) => {
+        return (
+          <Section key={index}>
+            <Link href={item.link} passHref>
+              <a target={item.newTab ? '_blank' : ''}>
+                <div
+                  style={{
+                    background: '#ffffff',
+                    textAlign: 'center',
+                    padding: '30px 25px',
+                    cursor: 'pointer'
+                  }}
+                >
+                  <h3
+                    style={{
+                      fontSize: '42px',
+                      fontWeight: 'bold',
+                      color: 'red'
+                    }}
+                  >
+                    {index + 1}
+                  </h3>
+                  <h3
+                    style={{
+                      fontWeight: 700,
+                      color: 'black',
+                      fontSize: '28px',
+                      marginBottom: '10px'
+                    }}
+                  >
+                    {item.title}
+                  </h3>
+                  <p style={{ fontSize: '16px', color: '#a7a7a7' }}>
+                    {item.description}
+                  </p>
+                </div>
+              </a>
+            </Link>
+          </Section>
+        )
+      })}
     </Container>
   </Layout>
 )
